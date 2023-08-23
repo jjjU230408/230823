ad3302a666fc0cc65406fbe367c75110567de525
jjjU230408 <130205480+jjjU230408@users.noreply.github.com>
2023-08-23 16:56:25 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
 import hashlib
 
-
 class Member:
     name = ""
     username = ""
     password = 1234
-
     def __init__(self, name, username, password):
         self.name = name
         self.username = username
         self.password = password
-        m = hashlib.sha256()
-
-        self.password = m.update(password.encode('utf-8'))
 
     def display(self):
-        print(f'이름 : {self.name}, 아이디 : {self.username}')
+       
+        print(f'이름 : {self.name}    아이디 : {self.username}')
 
 
 class Post:
@@ -25,8 +21,10 @@ class Post:
         self.author = author
 
 
-posts = []
 members = []
-members.append(Member('kim', 'kim123', 'password123'))
-members.append(Member('lee', 'lee456', 'password456'))
-members.append(Member('jo', 'jo789', 'password789'))
+members.append(Member("kim", "kim123", "password123"))
+members.append(Member("lee", "lee123", "password456"))
+members.append(Member("park", "park123", "password789"))
+
+for member in members:
+    member.display()
